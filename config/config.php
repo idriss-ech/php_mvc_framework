45e7d0373dd88f67d9f267e7d231c395657e5269
Idriss Echakouki <139574121+idriss-ech@users.noreply.github.com>
2025-02-19 10:15:43 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@
 
 define('DEVELOPMENT_ENVIRONMENT', true);
 
-define('DB_NAME', 'phpmvc');
+define('DB_NAME', 'db_name');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'void94701@');
+define('DB_PASSWORD', 'your_password');
 define('DB_HOST', 'localhost');
 
 
